7ef69c069a9ece66b660d4cd1842c4a47a0c6fac
DJ <social.deej@gmail.com>
2020-07-13 22:01:14 +0530

    Adding changes

--- app.js
+++ app.js
@@ -11,22 +11,23 @@ const user = 'ceceliacreates';
 const repo = 'APITools';
 
 app.get('/', (req, res) => {
-    var dataToSend;
-    // spawn new child process to call the python script
-    const python = spawn('python3', ['script.py', 'https://github.com/crusher-pb/Repository1']);
-    // collect data from script
-    python.stdout.on('data', function (data) {
-        console.log('Pipe data from python script ...');
-        dataToSend = data.toString();
-    });
-    // in close event we are sure that stream from child process is closed
-    python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
-        // send data to browser
-        console.log(dataToSend)
-        res.send(dataToSend)
-    });
-
+    // var dataToSend;
+    // // spawn new child process to call the python script
+    // const python = spawn('python3', ['script.py', 'https://github.com/crusher-pb/Repository1']);
+    // // collect data from script
+    // python.stdout.on('data', function (data) {
+    //     console.log('Pipe data from python script ...');
+    //     dataToSend = data.toString();
+    // });
+    // // in close event we are sure that stream from child process is closed
+    // python.on('close', (code) => {
+    //     console.log(`child process close all stdio with code ${code}`);
+    //     // send data to browser
+    //     console.log(dataToSend)
+    //     res.send(dataToSend)
+    // });
+    console.log("hello world")
+    res.send("hello world")
 })
 
 
