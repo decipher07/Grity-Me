df4a7daff31971e2b1bd6931e8a01f4c103e9d7b
DJ <social.deej@gmail.com>
2020-07-14 01:04:18 +0530

    Revert "Trying To Work with the API"
    This reverts commit fbd18a3ac00a09b0418ce2f59824ae9becc82d6b.

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const express = require('express')
-const {
-    spawn
-} = require('child_process');
+const {spawn} = require('child_process');
 const app = express()
 const PORT = process.env.PORT || 3000;
-const fetch = require('node-fetch');
-require('dotenv').config();
-const file = require('./TestIssues.json');
-const user = 'ceceliacreates';
-const repo = 'APITools';
 
 app.get('/', (req, res) => {
+
     var dataToSend;
     // spawn new child process to call the python script
-    const python = spawn('python3', ['script.py', 'https://github.com/crusher-pb/Repository1']);
+    const python = spawn('python', ['script.py', 'https://github.com/crusher-pb/Repository1']);
     // collect data from script
     python.stdout.on('data', function (data) {
         console.log('Pipe data from python script ...');
